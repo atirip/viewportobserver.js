a1e2ac092d585678a2ab7caae00a02b2907f0371
Priit Pirita <atirip@gmail.com>
2012-11-10 20:44:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,85 @@
+# viewportobserver.js
+
+Lean and mean scrollhandler to determine which part of observables are visible in viewport. Crucial part of every "lazyloader". `viewportobserver.js` is written in vanilla Javascript and has no dependancies.
+
+
+## Intro
+
+There are [many](http://www.appelsiini.net/projects/lazyload) and [many](http://css-tricks.com/snippets/javascript/lazy-loading-images/) obviosly good lazy load implemetations around. This is not yet another one. 
+
+The goals of viewobserver were:
+
+* independent, vanilla Javasctipt
+* just an observer, allow callbacks to handle DOM modifications, so not only images can be handled
+* squeeze the latest drop of fat out of the implementation - to achieve smoothest possible scrolling
+* allow reverse of loading, hiding too, important on mobile devices
+* prioritize elements directly visible in viewport, delay those inside threshold, but invisible 
+
+## Design desicions
+Iterating even over large array's is very fast, array modifications on the contrary (splice f.e.) are very slow. It is common approach in alternative implementations to gradually remove observables, after they are processed. I decided tehrefore, to speed things up,  not to actually remove observeable nodes from observer array, instead, if used in "lazyload mode", observables loaded are just deleted from that array ( `delete observables[i]` ). In the end, when f.e. all images are loaded, scroll handler still iterates every time over that empty array, but it only takes few milliseconds.  
+
+Scroll handler is throttled to the max on desktop computers. On mobile webkit devices only one scroll event at the end of scrolling movement is fired. The same approach on desktop computers as default. Though throttle is configurable to allow infrequent scroll events fired during scrolling too.  
+
+Most alternative implementations work in what I call "live" mode e.g call to determine observables position is made on every cycle. This is slow. If observables position inside page is not changed after they added to observe, there's no need to do that. `viewportobserver.js` works as default ion "non-live" mode where position is calculated only once - when node is added to observe.  
+
+Forgot events, they are slow. Iterate and process over array of observables instead of triggering events to them.
+
+
+## Usage
+
+**Include viewportobserver.js:**
+
+    <script src="path/to/viewportobserver.js" type="text/javascript"></script>
+
+**To add nodes to observe:**
+
+	atirip.viewportobserver( nodes, {
+		reveal: function(node) {},
+		hide: function(node) {},
+		live: false,
+		reach: 2,
+		
+		throttleDuringScroll: false,
+		throttleInterval: 200
+	})
+
+Where:  
+__nodes__ required, array of DOM nodes to observe ( jQuery result )  
+__reveal__ required, callback to reveal or load observable, one argument is passed to the callback - pure vanilla Javascript DOM Node  
+__hide__  optional, callback to hide or unload an observable, one argument is passed to the callback - pure vanilla Javascript DOM Node. When operating in "lazyload mode" do not set hide callback
+__live__ whether to calculate obsrevables positions on every iteration, if observables positions are fixed, non-live is faster, default is non-live e.g. false  
+__reach__ how many "pages" to keep visible, one page is window.height, so 2 keeps 5 "pages" visible, default is 2
+
+You can also change global parameters on every call to `atirip.viewportobserver()`, global parameters affect all nodes set to observe since changed:  
+__throttleDuringScroll__  whether to call scrollhandler during scroll, only effective on desktop computers, default false.  
+__throttleInterval__ throttle interval throttleDuringScroll set to true, in milliseconds, default 200ms
+  
+Currently `viewportobserver.js` only supports vertical scrolling viewport and `window` as viewport only. 
+ 
+## Demo
+
+#### To reveal & hide 
+
+Demo page [http://atirip.github.com/viewportobserver.js/observe.html](http://atirip.github.com/viewportobserver.js/observe.html) which holds 100 different (actually the same, but called with different GET paramaters) images (495p x 600px, 13Okb) each CSS GPU accelerated with `-webkit-transform: translate3d(0, 0, 0);`. This demo uses my [domhide.js](https://github.com/atirip/domhide.js) utility to hide DOM nodes. Scrolling is very smooth, uses not a lot of CPU power and more importantly - even old iOS devices do not crash.
+
+Theres another page [http://atirip.github.com/viewportobserver.js/lazy.html](http://atirip.github.com/viewportobserver.js/lazy.html) which implements the usual lazyload approach. Images (20, the same as above) are simply lazyloaded with `src` attribute change.
+
+## Compatible
+	
+Tested on Firefox 16, Chrome 23, Opera 12 on Mountain Lion and Windows, Safari 6 on Mountain Lion, IE 8, IE 7 on Windows (Windows XP under Parallels 7), iOS 6, Android 2.3
+
+## TODO
+
+* More testing on what are iOS limits on images
+* More test on Android
+* Test Windows Phone
+* Add horizontal scrolling support
+
+## Contact me
+
+For support, remarks and requests, please mail me at [atirip@yahoo.com](mailto:atirip@yahoo.com).
+
+## License
+
+Copyright (c) 2012 Priit Pirita, released under the MIT license.
+
---
+++ viewportobserver.js
@@ -0,0 +1,187 @@
+/*jshint laxcomma:true, laxbreak: true, asi:true */
+/*
+* MIT Licensed
+* Copyright (c) 2012, Priit Pirita, atirip@yahoo.com
+* https://github.com/atirip/bodyscrollkiller.js
+*/
+
+;(function(window, APP) {
+
+	var reachDefault = 2 // how many "pages" to keep visible, one page is window.height, so 2 keeps 5 "pages" visible
+	,	liveDefault = false // whether to ask getBoundingClientRect always, or only once
+	,	throttleDuringScroll = false
+	,	throttleInterval = 200
+	,	doc = window.document
+	,	docElem = doc.documentElement
+	,	body = doc.body
+	,	slice = Array.prototype.slice
+	,	observables = []
+
+	,	scrollHandler = function() {
+			var run = function() {
+				var height = (doc.compatMode === "CSS1Compat" && docElem.clientHeight || body && body.clientHeight || docElem.clientHeight)||800
+				,	top = docElem.scrollTop || body.scrollTop
+				,	i = 0
+				,	o
+				,	l = observables.length
+
+				// we could benefit educated quess here, which ones not to loop at all ?
+				for(; i < l; i++) {
+					if ( (o = observables[i]) && false === o.call(null, height, top) ) {
+							// it is not worth to do anything more,
+							// iterating over huge empty array is lightning fast
+							// so we just delete and even do not remove scroll handler
+							// array.splice would remove elements, but it is SLOW
+							// so this approach is faster in the end, even when all are revealed
+							delete observables[i]
+					}
+				}
+
+			}
+			setTimeout(run, 10)
+		}
+	
+	,	throttle = function (interval, fn) {
+			var lastTime = null
+			,	timer = null
+
+			return function () {
+				var self = this
+				,	args = slice.call(arguments, 0)
+				,	func = function () {
+						fn.apply(self, args)
+						lastTime = null
+					}
+				,	run = function () {
+						if ( throttleDuringScroll ) {
+							var now = new Date
+							if (now - lastTime > interval) {
+								// if interval passed, call
+								lastTime = now
+								fn.apply(self, args)
+							}
+						}
+						// call once after last ettempt
+						timer && clearTimeout(timer)
+						timer = setTimeout(func, interval)
+					}
+				setTimeout(run, 10)
+			}
+		}
+	
+		/*
+			nodes - array or array like nodeset ( may be jquery result )
+			optioos {
+				reach - reach in pages, where one page is viewport height, reach 2 gives us 5 pages - actual viewport + 2 at top and 2 at bottom
+				live - if nodes offset in page is possibly changing, set live to true (slower)
+				reveal - function to reaveal node, takes one parameter, the node
+				hide - function to hide node, takes one parameter, the node, do not set in "lazyload" setup
+				
+				you can change global settings too
+				throttleDuringScroll - default is false
+				throttleInterval - default is 200
+				
+			}
+		*/
+	,	observe = function(nodes, options) {
+			options = options || {}
+			// set globals, on both 0 || false is valid setting
+			;(undefined !== options.throttleDuringScroll) && (throttleDuringScroll = options.throttleDuringScroll)
+			;(undefined !== options.throttleInterval) && (throttleInterval = options.throttleInterval)
+	
+			var hide = options.hide
+			,	reveal = options.reveal ? function(node, timeout) {
+					var callback = function() { options.reveal(node) }
+					;( undefined === timeout ) ? options.reveal(node) : setTimeout(callback, timeout)
+				} : function() {}
+			,	reach = options.reach || reachDefault
+			,	live = options.live || liveDefault
+			,	len = +(nodes && nodes.length)||0
+			,	i = 0
+			,	node
+			,	rect
+			,	top = docElem.scrollTop || body.scrollTop
+	
+			for(; i < len; i++) {
+				node = nodes[i]
+				if ( !live ) {
+					rect = node.getBoundingClientRect()
+					// ? for some reason ClientRect seems to be read-only on iOS ...
+					rect = {
+						// adjust it accordingly to window scrollpos
+						top: rect.top + top,
+						bottom: rect.bottom + top
+					}
+				}
+				observables[observables.length] = (function(node, live, reveal, hide, rect, reach){
+
+					return function(height, top) {
+
+						var mtop
+						,	mbot
+						,	vtop
+						,	vbot
+
+						if ( height.observeHeight ) {
+							top = height.observeTop
+							height = height.observeHeight
+						}
+
+						mtop = -reach * height // full area to reveal
+						mbot = -mtop + height
+						vtop = 0 // directly visible area
+						vbot = height
+							
+						if ( live ) {
+							rect = node.getBoundingClientRect()
+						} else {
+							mtop = top + mtop
+							mbot = top + mbot
+							vtop = top
+							vbot = top + vbot
+						}
+
+						// element is visible when its top < max bottom && bottom > max top
+						if ( rect.bottom > mtop && rect.top < mbot ) {
+							// throw not visible nodes out of the thread, then they execute later
+							reveal.call(window, node, rect.bottom > vtop && rect.top < vbot ? undefined : 10)
+							if ( !hide ) {
+								// false signals to remove this observable
+								return false
+							}
+						} else {
+							hide && hide.call(window, node)
+						}
+						return true
+					}
+				})(node, live, reveal, hide, rect, reach) // close all in
+
+			}
+			scrollHandler()
+		}
+	
+	,	scrollThrottle = (function() {
+			if ( 'ontouchstart' in window ) {
+				// on touch devices we have only one scroll event during scrolling, at the end
+				return scrollHandler
+			} else {
+				// fire after throttleInterval since last scroll event
+				return throttle(throttleInterval, scrollHandler)
+			}
+		})()
+
+	// only FF < 3 actually
+	if ( !("getBoundingClientRect" in docElem) ) {
+		return
+	}
+
+	// we only add scroll event handler, never remove
+	if ( window.addEventListener ) {
+		window.addEventListener('scroll', scrollThrottle, false)
+	} else if( window.attachEvent ) {
+		window.attachEvent('onscroll', scrollThrottle)
+	}
+
+	APP.viewportobserver = observe
+
+})(window, (window.atirip || (window.atirip = {}) ));
